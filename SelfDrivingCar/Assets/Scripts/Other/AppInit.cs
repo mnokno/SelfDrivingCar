0100e2caa537b9c325052e840956d47db94c8a02
mnokno <prv.nowacki@gmail.com>
2023-03-19 17:29:37 +0000

    Removed UI used for debugging

--- SelfDrivingCar/Assets/Scripts/Other/AppInit.cs
+++ SelfDrivingCar/Assets/Scripts/Other/AppInit.cs
@@ -6,28 +6,8 @@ using UnityEngine.UI;
 
 public class AppInit : MonoBehaviour
 {
-    public InputField input;
-
     private void Start()
     {
-        input.text = "HERE 1";
-        HDebug();
         DB_Init.CheckTables();
-        HDebug();
-    }
-
-    public void HDebug()
-    {
-        input.text = "HERE 2";
-        //text.text = "URI=file:" + Application.dataPath + "/DB/DeepNet.db";  //Path to database
-        try
-        {
-            //input.text = Application.persistentDataPath;
-            input.text = DB_Init.CheckIfTableExists("Network").ToString();
-        }
-        catch (Exception e)
-        {
-            input.text = e.ToString();
-        }
     }
 }
